26f7d3acd7b63d8308b00df86e2329146b067b23
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-10-19 22:40:13 +0300

    изменения

--- src/main/java/pro/sky/calculator/service/CalculatorService.java
+++ src/main/java/pro/sky/calculator/service/CalculatorService.java
@@ -31,6 +31,8 @@ public final class CalculatorService {
     public String divide(Integer num1, Integer num2) {
         if (num1 == null || num2 == null) {
             return "Неверно введен запрос, попробуйте еще раз";
+        } else if(num2 == null) {
+            return "Ошибка! Делить на ноль нельзя";
         } else {
             return num1 + " / " + num2 + " = " + (num1/num2);
         }
